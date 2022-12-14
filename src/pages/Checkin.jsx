7e9f9d75aeb83b8870d55d13cfb1bbc6d23acd8d
Nasuco <2011501026@student.unisayogya.ac.id>
2022-12-14 16:20:02 +0700

    add ui responsive detailpembelian

--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -48,7 +48,7 @@ export default function Checkin() {
                       />
                     </div>
                     <div className='row col-10 mb-5'>
-                      <label hrmlFor='nomorRef' className='form-label text-primary'>Nomor Referensi</label>
+                      <label htmlFor='nomorRef' className='form-label text-primary'>Nomor Referensi</label>
                       <input
                         type='number'
                         className='form-control'
@@ -80,71 +80,6 @@ export default function Checkin() {
           </div>
         </div>
       </div>
-      {/* <img
-        src='/assets/img/Image 25.svg'
-        alt=''
-        className='position-absolute img-checkin'
-      />
-      <div className='position-relative text-center pt-5'>
-        <h2 className='text-light'>IDENTIFIKASI</h2>
-        <p className='text-light'>
-          Mulai check-in dengan tanggal keberangkatan dan referensi <br></br>
-          pemesanan atau nomor e-tiket Anda
-        </p>
-      </div>
-      <div
-        className='wrapper-login mx-auto position-relative'
-        style={{ width: '800px', marginTop: '150px' }}
-      >
-        <div className='title mt-5 pt-5  fw-bold'>Detail Anda</div>
-        <form action=''>
-          <div className='row'>
-            <div className='col-sm-6'>
-              <div className='field'>
-                <label htmlFor='namadepan'>Tiket Penerbangan</label>
-                <br />
-                <input
-                  type='text'
-                  placeholder='Masukkan Kode Tiket Anda'
-                  required
-                />
-              </div>
-
-              <br />
-              <br />
-              <div className='field button mt-5'>
-                <Link className='active' to='/hasilcheckin'>
-                  <input
-                    type='submit'
-                    value={'Periksa Tiket'}
-                    className='bg-primary text-light'
-                  />
-                </Link>
-              </div>
-            </div>
-
-            <div className='col-sm-6'>
-              <h6
-                className='fw-bold text-center'
-                style={{ marginTop: '50px', color: '#0075CF' }}
-              >
-                SCAN QR
-              </h6>
-              <img
-                src='/assets/img/Group 11.svg'
-                alt=''
-              />
-              <h6
-                className='fw-bold text-center'
-                style={{ marginTop: '30px', color: '#808184' }}
-              >
-                Scan QR menggunakan Hp <br />
-                Anda
-              </h6>
-            </div>
-          </div>
-        </form>
-      </div> */}
     </div>
   )
 }

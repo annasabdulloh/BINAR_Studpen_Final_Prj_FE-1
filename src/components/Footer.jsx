a00a5db20ff3001535375c395464b80461e26a15
faishalazka_ <fshlzkmr12@gmail.com>
2022-12-14 23:37:27 +0700

    update progress responsive #1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
     <div className='foot'>
       <div className='container mb-5'>
         <div className='row'>
-          <div className='col-3'>
+          <div className='col-md-3'>
             <h2 className='fw-bold'>MyAIRFARE</h2>
           </div>
-          <div className='col-3'>
+          <div className='col-md-3'>
             <p>Tentang BeLight</p>
             <p>Procurement Online</p>
             <p>Hubungan Investor</p>
@@ -16,7 +16,7 @@ const Footer = () => {
             <p>PPID MyAIRFARE</p>
             <p>Pasca Homologasi</p>
           </div>
-          <div className='col-3'>
+          <div className='col-md-3'>
             <p>Mitra</p>
             <p>Corporate Privilege</p>
             <p>Customer Care Polic</p>
@@ -26,23 +26,44 @@ const Footer = () => {
             <p>Peta Situs</p>
             <p>Hubungi Kami</p>
           </div>
-          <div className='col-3'>
+          <div className='col-md-3 before-end'>
             <h5>Connect With Us</h5>
-            <img src='/assets/img/image 19.svg' className='mt-5'></img>
-            <img src='/assets/img/image 20.svg' className='mt-3'></img>
+            <img
+              src='/assets/img/image 19.svg'
+              className='mt-5'
+              style={{ maxWidth: '100%' }}
+            ></img>
+            <br />
+            <img
+              src='/assets/img/image 20.svg'
+              className='mt-5'
+              style={{ maxWidth: '100%' }}
+            ></img>
           </div>
         </div>
       </div>
 
       <div className='bg-primary m-0 p-5'>
         <div className='row'>
-          <div className='col-6'>
-            <img src='/assets/img/image 20.svg'></img>
-          </div>
-          <div className='col-6'>
+          <div className='col-lg-4'>
             <img
-              src='/assets/img/Group 7.svg'
-              style={{ maxWidth: '100%' }}
+              src='/assets/img/Group 10.svg'
+              style={{ Width: '100%' }}
+              className='group-10'
+            ></img>
+          </div>
+          <div className='col-lg-4 mt-3'>
+            <img
+              src='/assets/img/image 21.svg'
+              style={{ Width: '100%' }}
+              className='group-11'
+            ></img>
+          </div>
+          <div className='col-lg-4 mt-3'>
+            <img
+              src='/assets/img/image 22.svg'
+              style={{ Width: '100%' }}
+              className='group-11'
             ></img>
           </div>
         </div>

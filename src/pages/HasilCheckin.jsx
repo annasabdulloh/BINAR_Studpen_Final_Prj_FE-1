b4522a81509945f1e770da306937d8f11b00fd03
Nasuco <2011501026@student.unisayogya.ac.id>
2022-12-14 16:26:59 +0700

    update detailpembelian

--- src/pages/HasilCheckin.jsx
+++ src/pages/HasilCheckin.jsx
@@ -7,16 +7,19 @@ export default function HasilCheckin() {
                     bg-image
                     p-3
                     mb-4'
-        style={{ backgroundImage: 
-          'url("./assets/img/image 25.svg")' }}
+        style={{ backgroundImage: 'url("./assets/img/image 25.svg")' }}
       >
-        <div className='d-flex 
+        <div
+          className='d-flex 
                           justify-content-center
                           align-items-center 
-                          h-100'>
+                          h-100'
+        >
           <div>
-            <h2 className='text-light
-                            fw-bold'>
+            <h2
+              className='text-light
+                            fw-bold'
+            >
               Tiket Anda
             </h2>
           </div>
@@ -24,207 +27,244 @@ export default function HasilCheckin() {
       </div>
 
       {/* Body */}
-      <div className="col-md-12 d-flex 
+      <div
+        className='col-md-12 d-flex 
                         justify-content-center 
                         align-items-center
-                        p-3">
-        <div className="col-md-8 my-3">
+                        p-3'
+      >
+        <div className='col-md-8 my-3'>
           <img
-              style={{width: 200}}
-              src='./assets/images/logo.png' 
-              className='logo'
+            style={{ width: 200 }}
+            src='./assets/images/logo.png'
+            className='logo'
           />
         </div>
-        <div className="d-flex
+        <div
+          className='d-flex
                         justify-content-center
                         align-items-center
-                        p-3">
-          <div className="col-md-4 my-3">
-            <h3>
-              E-Ticket
-            </h3>
+                        p-3'
+        >
+          <div className='col-md-4 my-3'>
+            <h3>E-Ticket</h3>
           </div>
         </div>
       </div>
 
       {/* Body-Rectangle */}
-      <div className="row d-flex
+      <div
+        className='row d-flex
                       justify-content-center
                       align-content-center 
-                      p-3">
-        <div className="col-sm-10">
-          <div className="card g-3 
+                      p-3'
+      >
+        <div className='col-sm-10'>
+          <div
+            className='card g-3 
                           p-3
-                          mx-auto">
-            <div className="col-12">
-              <div className="row pt-4 
+                          mx-auto'
+          >
+            <div className='col-12'>
+              <div
+                className='row pt-4 
                               justify-content-center
                               align-content-center 
-                              p-3">
-                  <div className="col-md-3 
-                                  my-3">
-                    <img
-                        style={{width: 130}}
-                        src='./assets/images/logo.png' 
-                        className='logo'
-                    />
+                              p-3'
+              >
+                <div
+                  className='col-md-3 
+                                  my-3'
+                >
+                  <img
+                    style={{ width: 130 }}
+                    src='./assets/images/logo.png'
+                    className='logo'
+                  />
+                </div>
+                <div
+                  className='col-md-2 
+                                  my-3'
+                >
+                  <div className='fw-bold'>Nama Pemesan</div>
+                  <div>Andi Rayhan</div>
+                </div>
+                <div
+                  className='col-md-3 
+                                  my-3'
+                >
+                  <div className='fw-bold'>Tanggal Pesan</div>
+                  <div>Jum&#39;at&apos; 25 Desember 2022</div>
+                </div>
+                <div
+                  className='col-md-2 
+                                  my-3'
+                >
+                  <div className='fw-bold'>Nomor E-Ticket</div>
+                  <div
+                    className='fw-bold 
+                                    text-primary'
+                  >
+                    3xvv2H
                   </div>
-                  <div className="col-md-2 
-                                  my-3">
-                    <div className="fw-bold">
-                      Nama Pemesan
-                    </div>
-                    <div>Andi Rayhan</div>
-                  </div>
-                  <div className="col-md-3 
-                                  my-3">
-                    <div className="fw-bold">
-                      Tanggal Pesan
-                    </div>
-                    <div>Jum&#39;at&apos; 25 Desember 2022</div>
-                  </div>
-                  <div className="col-md-2 
-                                  my-3">
-                    <div className="fw-bold">
-                      Nomor E-Ticket
-                    </div>
-                    <div className="fw-bold 
-                                    text-primary">
-                      3xvv2H
-                    </div>
-                  </div>
-                <div className="row
+                </div>
+                <div
+                  className='row
                                 pt-3
-                                mb-4">
-                  <div className="col-md-4 
-                                  my-3">
-                    <div className="card g-3 p-3 mb-1 
+                                mb-4'
+                >
+                  <div
+                    className='col-md-4 
+                                  my-3'
+                  >
+                    <div
+                      className='card g-3 p-3 mb-1 
                                     text-center 
                                     fw-bold 
                                     bg-secondary 
                                     bg-opacity-25 
-                                    text-primary">
+                                    text-primary'
+                    >
                       Nomor Penerbangan
                     </div>
-                    <div className="card g-3 p-3 
+                    <div
+                      className='card g-3 p-3 
                                     bg-secondary 
-                                    bg-opacity-10">
+                                    bg-opacity-10'
+                    >
                       <div>GA 640/GA 7640</div>
                       <div>Kelas : Ekonomi</div>
                     </div>
                   </div>
-                  <div className="col-md-4 my-3">
-                    <div className="card g-3 p-1 mb-1 
+                  <div className='col-md-4 my-3'>
+                    <div
+                      className='card g-3 p-1 mb-1 
                                     text-center 
                                     fw-bold 
                                     bg-secondary 
-                                    bg-opacity-25">
-                      <div className=" text-primary">Keberangkatan</div>
+                                    bg-opacity-25'
+                    >
+                      <div className=' text-primary'>Keberangkatan</div>
                       <div>Minggu, 27 Desember 2022</div>
                     </div>
-                    <div className="card g-3 p-3 
+                    <div
+                      className='card g-3 p-3 
                                     bg-secondary 
-                                    bg-opacity-10">
-                      <div>Jakarta-cengkareng(CGK)
-                          Suekarno Hatta International
-                          Airport
+                                    bg-opacity-10'
+                    >
+                      <div>
+                        Jakarta-cengkareng(CGK) Suekarno Hatta International
+                        Airport
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-4 my-3">
-                    <div className="card g-3 p-1 mb-1 
+                  <div className='col-md-4 my-3'>
+                    <div
+                      className='card g-3 p-1 mb-1 
                                     text-center 
                                     fw-bold 
                                     bg-secondary 
-                                    bg-opacity-25">
-                      <div className=" text-primary">Kedatangan</div>
+                                    bg-opacity-25'
+                    >
+                      <div className=' text-primary'>Kedatangan</div>
                       <div>Minggu, 27 Desember 2022</div>
                     </div>
-                    <div className="card g-3 p-3 
+                    <div
+                      className='card g-3 p-3 
                                     bg-secondary 
-                                    bg-opacity-10">
+                                    bg-opacity-10'
+                    >
                       <div>Lombok International Airport</div>
                     </div>
                   </div>
                 </div>
-                <div class="p-3 mb-2 
+                <div
+                  className='p-3 mb-2 
                             bg-primary 
                             text-white 
                             fw-bold 
-                            text-center">
+                            text-center'
+                >
                   Detail Penumpang
                 </div>
-                <table className="col-md-3 table 
+                <table
+                  className='col-md-3 table 
                                   justify-content-center 
                                   align-content-center 
-                                  mb-5">
-                  <thead className="table-primary 
-                                    text-primary">
+                                  mb-5'
+                >
+                  <thead
+                    className='table-primary 
+                                    text-primary'
+                  >
                     <tr>
-                      <th scope="col">NO</th>
-                      <th scope="col">Title</th>
-                      <th scope="col">Nama Penumpang</th>
-                      <th scope="col">Kelas</th>
-                      <th scope="col">No.Kursi</th>
+                      <th scope='col'>NO</th>
+                      <th scope='col'>Title</th>
+                      <th scope='col'>Nama Penumpang</th>
+                      <th scope='col'>Kelas</th>
+                      <th scope='col'>No.Kursi</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <th scope="col">1</th>
-                      <th scope="col">Mr</th>
-                      <th scope="col">Andi Rayhan</th>
-                      <th scope="col">Ekonomi</th>
-                      <th scope="col">104</th>
+                      <th scope='col'>1</th>
+                      <th scope='col'>Mr</th>
+                      <th scope='col'>Andi Rayhan</th>
+                      <th scope='col'>Ekonomi</th>
+                      <th scope='col'>104</th>
                     </tr>
                   </tbody>
                 </table>
               </div>
-              <div className="text-center">
-                MyAIRFARE INDONESIA,
-                JALAN RAYA SIRIH NO. 21 JAKARTA
+              <div className='text-center'>
+                MyAIRFARE INDONESIA, JALAN RAYA SIRIH NO. 21 JAKARTA
               </div>
             </div>
           </div>
-          <div className="col-md-12 d-flex 
+          <div
+            className='col-md-12 d-flex 
                               justify-content-center 
                               align-items-center
-                              p-3">
-                  <div className="col-md-5 my-3">
-                    Anda dapat menyimpan dan mencetak tiket
-                  </div>
-                  <div className="row pt-10 d-flex
+                              p-3'
+          >
+            <div className='col-md-5 my-3'>
+              Anda dapat menyimpan dan mencetak tiket
+            </div>
+            <div
+              className='row pt-10 d-flex
                                   justify-content-center
                                   align-items-center
-                                  p-3">
-                    <div className="col-md-4 my-3">
-                      <img 
-                        src="/assets/img/image 37.svg">
-                      </img>
-                      <div className='btn btn-primary 
-                                      btn-checkin'>
-                        Simpan
-                      </div>
-                    </div>
-                    <div className="col-md-4 my-3">
-                      <img 
-                        src="/assets/img/image 35.svg">
-                      </img>
-                      <div className='btn btn-primary 
-                                      btn-checkin'>
-                        Cetak Tiket
-                      </div>
-                    </div>
-                    <div className="col-md-4 my-3">
-                      <img
-                        src="/assets/img/image 36.svg">
-                      </img>
-                      <div className='btn btn-primary
-                                      btn-checkin'>
-                        Kirim Via E-Mail
-                      </div>
-                    </div>
-                  </div>
+                                  p-3'
+            >
+              <div className='col-md-4 my-3'>
+                <img src='/assets/img/image 37.svg'></img>
+                <div
+                  className='btn btn-primary 
+                                      btn-checkin'
+                >
+                  Simpan
+                </div>
+              </div>
+              <div className='col-md-4 my-3'>
+                <img src='/assets/img/image 35.svg'></img>
+                <div
+                  className='btn btn-primary 
+                                      btn-checkin'
+                >
+                  Cetak Tiket
+                </div>
+              </div>
+              <div className='col-md-4 my-3'>
+                <img src='/assets/img/image 36.svg'></img>
+                <div
+                  className='btn btn-primary
+                                      btn-checkin'
+                >
+                  Kirim Via E-Mail
+                </div>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/tentangKami.jsx
+++ src/pages/tentangKami.jsx
@@ -1,31 +1,39 @@
-
 export default function TentangKami() {
   return (
     <div>
-        <img src='../../public/assets/images/girl.png' className='img-fluid mb-1'></img>
-        <div className='p-5'>
-            <div className='mb-4'>
-                <h5 className='text-primary fw-bold'>PROFILE PERUSAHAAN</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Officia eius cumque aliquid itaque error totam fuga, 
-                    magnam excepturi quae suscipit dolorum architecto temporibus facilis maxime delectus amet, ex sequi animi!
-                </p>
-            </div>
-            <div className='mb-4'>
-                <h5 className='text-primary fw-bold'>VISI MISI PERUSAHAAN</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Officia eius cumque aliquid itaque error totam fuga, 
-                    magnam excepturi quae suscipit dolorum architecto temporibus facilis maxime delectus amet, ex sequi animi!
-                </p>
-            </div>
-            <div>
-                <h5 className='text-primary fw-bold'>PENGHARGAAN</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Officia eius cumque aliquid itaque error totam fuga, 
-                    magnam excepturi quae suscipit dolorum architecto temporibus facilis maxime delectus amet, ex sequi animi!
-                </p>
-            </div>
+      <img
+        src='../../public/assets/images/girl.png'
+        className='img-fluid mb-1'
+      ></img>
+      <div className='p-5'>
+        <div className='mb-4'>
+          <h5 className='text-primary fw-bold'>PROFILE PERUSAHAAN</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            eius cumque aliquid itaque error totam fuga, magnam excepturi quae
+            suscipit dolorum architecto temporibus facilis maxime delectus amet,
+            ex sequi animi!
+          </p>
         </div>
+        <div className='mb-4'>
+          <h5 className='text-primary fw-bold'>VISI MISI PERUSAHAAN</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            eius cumque aliquid itaque error totam fuga, magnam excepturi quae
+            suscipit dolorum architecto temporibus facilis maxime delectus amet,
+            ex sequi animi!
+          </p>
+        </div>
+        <div>
+          <h5 className='text-primary fw-bold'>PENGHARGAAN</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            eius cumque aliquid itaque error totam fuga, magnam excepturi quae
+            suscipit dolorum architecto temporibus facilis maxime delectus amet,
+            ex sequi animi!
+          </p>
+        </div>
+      </div>
     </div>
   )
 }

b4ae7472bcb85ab5512d4b7931bb7c72cb280f89
Nasuco <2011501026@student.unisayogya.ac.id>
2022-12-16 16:14:32 +0700

    update responsive notifada

--- src/pages/Notifikasi/NotifAda.jsx
+++ src/pages/Notifikasi/NotifAda.jsx
@@ -9,88 +9,85 @@ function NotifAda() {
       >
         <div className='d-flex justify-content-center align-items-center h-100'>
           <div>
-            <h3 className='text-light fw-bold'>
-              Notifikasi
-            </h3>
+            <h3 className='text-light fw-bold'>Notifikasi</h3>
           </div>
         </div>
       </div>
       <div className='row d-flex justify-content-center align-content-center p-3'>
         <div className='col-sm-3 p-3 pt-0'>
-          
           <div className='p-3 bg-white fs-5 border border-secondary justify-content-center align-items-center bg-opacity-50'>
             <div className='p-2 bg-success text-light text-center fs-4 border-0 d-flex justify-content-center align-items-center'>
               <div className='col-md-8 my-2 fs-5'>Tandai yang dibaca</div>
             </div>
             <div>
-                <div className='fw-bold mb-3'>
-                  Status
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input me-2'
-                      type='checkbox'
-                      name='listNotif'
-                      value=''
-                      id='firstNotif'
-                    />
-                    <label
-                      className='form-check-label text-Capitalize fw-normal'
-                      htmlFor='firstNotif flexCheckDefault'
-                    >
-                      Sudah Dibaca
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input me-2'
-                      type='checkbox'
-                      name='listNotif'
-                      value=''
-                      id='firstNotif'
-                    />
-                    <label
-                      className='form-check-label text-Capitalize fw-normal'
-                      htmlFor='firstNotif flexCheckDefault'
-                    >
-                      Belum Dibaca
-                    </label>
-                  </div>
+              <div className='fw-bold mb-3'>
+                Status
+                <div className='form-check'>
+                  <input
+                    className='form-check-input me-2'
+                    type='checkbox'
+                    name='listNotif'
+                    value=''
+                    id='firstNotif'
+                  />
+                  <label
+                    className='form-check-label text-Capitalize fw-normal'
+                    htmlFor='firstNotif flexCheckDefault'
+                  >
+                    Sudah Dibaca
+                  </label>
+                </div>
+                <div className='form-check'>
+                  <input
+                    className='form-check-input me-2'
+                    type='checkbox'
+                    name='listNotif'
+                    value=''
+                    id='firstNotif'
+                  />
+                  <label
+                    className='form-check-label text-Capitalize fw-normal'
+                    htmlFor='firstNotif flexCheckDefault'
+                  >
+                    Belum Dibaca
+                  </label>
                 </div>
               </div>
-              <div>
-                <div className='fw-bold'>
-                  Urut Berdasarkan
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input me-2'
-                      type='checkbox'
-                      name='listNotif'
-                      value=''
-                      id='firstNotif'
-                    />
-                    <label
-                      className='form-check-label text-Capitalize fw-normal'
-                      htmlFor='firstNotif flexCheckDefault'
-                    >
-                      Terbaru
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input me-2'
-                      type='checkbox'
-                      name='listNotif'
-                      value=''
-                      id='firstNotif'
-                    />
-                    <label
-                      className='form-check-label text-Capitalize fw-normal'
-                      htmlFor='firstNotif flexCheckDefault'
-                    >
-                      Terlama
-                    </label>
-                  </div>
+            </div>
+            <div>
+              <div className='fw-bold'>
+                Urut Berdasarkan
+                <div className='form-check'>
+                  <input
+                    className='form-check-input me-2'
+                    type='checkbox'
+                    name='listNotif'
+                    value=''
+                    id='firstNotif'
+                  />
+                  <label
+                    className='form-check-label text-Capitalize fw-normal'
+                    htmlFor='firstNotif flexCheckDefault'
+                  >
+                    Terbaru
+                  </label>
                 </div>
+                <div className='form-check'>
+                  <input
+                    className='form-check-input me-2'
+                    type='checkbox'
+                    name='listNotif'
+                    value=''
+                    id='firstNotif'
+                  />
+                  <label
+                    className='form-check-label text-Capitalize fw-normal'
+                    htmlFor='firstNotif flexCheckDefault'
+                  >
+                    Terlama
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -98,35 +95,35 @@ function NotifAda() {
           <div className='card g-3 p-5 mx-auto rounded-0 border bg-danger bg-opacity-10'>
             <div className='shadow mb-3 bg-body rounded p-3'>
               <div className='text-primary fs-5'>Pembelian Tiket</div>
-              Anda sudah memesan tiket, silahkan dilanjutkan untuk 
-              pembayarandengan metode yang anda gunakan, 
-              pastikan data anda sudah benar
+              Anda sudah memesan tiket, silahkan dilanjutkan untuk
+              pembayarandengan metode yang anda gunakan, pastikan data anda
+              sudah benar
             </div>
             <div className='shadow mb-3 bg-body rounded p-3'>
               <div className='text-primary fs-5'>Pembelian Tiket</div>
-              Anda sudah memesan tiket, silahkan dilanjutkan untuk 
-              pembayarandengan metode yang anda gunakan, 
-              pastikan data anda sudah benar
+              Anda sudah memesan tiket, silahkan dilanjutkan untuk
+              pembayarandengan metode yang anda gunakan, pastikan data anda
+              sudah benar
             </div>
             <div className='shadow mb-5 bg-body rounded p-3'>
               <div className='text-primary fs-5'>Pembelian Tiket</div>
-              Anda sudah memesan tiket, silahkan dilanjutkan untuk 
-              pembayarandengan metode yang anda gunakan, 
-              pastikan data anda sudah benar
+              Anda sudah memesan tiket, silahkan dilanjutkan untuk
+              pembayarandengan metode yang anda gunakan, pastikan data anda
+              sudah benar
             </div>
             <div className='d-flex justify-content-center p-3'>
-                <div className='position-absolute bottom-0 end-0 p-4'>
-                  <Link to='#' className='p-3'>
-                    <img
-                      src='./assets/images/remove.png'
-                      style={{ width: 30, height: 30 }}
-                    />
-                  </Link>
-                  <Link to='#' className='btn btn-primary'>
-                    Kembali ke Dashboard
-                  </Link>
-                </div>
+              <div className='position-absolute bottom-0 end-0 p-4'>
+                <Link to='#' className='p-3'>
+                  <img
+                    src='./assets/images/remove.png'
+                    style={{ width: 30, height: 30 }}
+                  />
+                </Link>
+                <Link to='#' className='btn btn-primary'>
+                  Kembali ke Dashboard
+                </Link>
               </div>
+            </div>
           </div>
         </div>
       </div>
